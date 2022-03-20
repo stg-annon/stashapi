8f0e4275d73d63e19b2afc64894b33c8d92808d9
stg-annon <14135675+stg-annon@users.noreply.github.com>
2022-03-20 18:59:50 -0400

    add GQLWrapper class

---
+++ classes.py
@@ -0,0 +1,72 @@
+import re, sys
+from collections import defaultdict
+
+import requests
+
+import logging as log
+log.basicConfig(level=log.DEBUG, format='%(levelname)8s: %(message)s')
+
+
+class GQLWrapper:
+	port = ""
+	url = ""
+	headers = {
+		"Accept-Encoding": "gzip, deflate",
+		"Content-Type": "application/json",
+		"Accept": "application/json",
+		"Connection": "keep-alive",
+		"DNT": "1"
+	}
+	cookies = {}
+
+	def __init__(self):
+		return
+
+	def __resolveFragments(self, query):
+		fragmentReferences = list(set(re.findall(r'(?<=\.\.\.)\w+', query)))
+		fragments = []
+		for ref in fragmentReferences:
+			fragments.append({
+				"fragment": ref,
+				"defined": bool(re.search("fragment {}".format(ref), query))
+			})
+
+		if all([f["defined"] for f in fragments]):
+			return query
+		else:
+			for fragment in [f["fragment"] for f in fragments if not f["defined"]]:
+				if fragment not in self.fragments:
+					raise Exception(f'GraphQL error: fragment "{fragment}" not defined')
+				query += self.fragments[fragment]
+			return self.__resolveFragments(query)
+
+	def _callGraphQL(self, query, variables=None):
+
+		query = self.__resolveFragments(query)
+
+		json_request = {'query': query}
+		if variables is not None:
+			json_request['variables'] = variables
+
+		response = requests.post(self.url, json=json_request, headers=self.headers, cookies=self.cookies)
+		
+		if response.status_code == 200:
+			result = response.json()
+
+			if result.get("errors"):
+				for error in result["errors"]:
+					log.error(f"GraphQL error: {error}")
+			if result.get("error"):
+				for error in result["error"]["errors"]:
+					log.error(f"GraphQL error: {error}")
+			if result.get("data"):
+				scraped_markers = defaultdict(lambda: None)
+				scraped_markers.update(result)
+				return scraped_markers['data']
+		elif response.status_code == 401:
+			sys.exit("HTTP Error 401, Unauthorized. Cookie authentication most likely failed")
+		else:
+			raise ConnectionError(
+				"GraphQL query failed:{} - {}. Query: {}. Variables: {}".format(
+					response.status_code, response.content, query, variables)
+			)
