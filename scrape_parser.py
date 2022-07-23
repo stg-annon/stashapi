b4a026d663547f5b98b0df6d07191ab9ca5fb890
stg-annon <14135675+stg-annon@users.noreply.github.com>
2022-07-23 04:26:21 -0400

    update parser to return new dicts with specific attrs

--- scrape_parser.py
+++ scrape_parser.py
@@ -50,13 +50,14 @@ class ScrapeParser:
 			child_ids: [ID!]
 		}
 		"""
-		if tag.get("stored_id"):
-			tag["id"] = tag["stored_id"]
-			del tag["stored_id"]
-		elif self.create_missing_tags:
-			return self.stash.create_tag({"name":tag["name"]})
+		tag_update = {}
 
-		return clean_dict(tag)
+		if tag.get("stored_id"):
+			tag_update["id"] = tag["stored_id"]
+		elif self.create_missing_tags:
+			return self.stash.find_tag(tag["name"], create=True)
+
+		return tag_update
 
 	def gallery_from_scrape(self, gallery):
 		"""
@@ -86,18 +87,16 @@ class ScrapeParser:
 			performer_ids: [ID!]
 		}
 		"""
+		gallery_update = {}
 
 		if gallery.get("tags"):
-			gallery["tag_ids"] = [self.tag_from_scrape(t)["id"] for t in gallery["tags"]]
-			del gallery["tags"]
+			gallery_update["tag_ids"] = [self.tag_from_scrape(t)["id"] for t in gallery["tags"]]
 		if gallery.get("performers"):
-			gallery["performer_ids"] = [self.performer_from_scrape(p)["id"] for p in gallery["performers"]]
-			del gallery["performers"]
+			gallery_update["performer_ids"] = [self.performer_from_scrape(p)["id"] for p in gallery["performers"]]
 		if gallery.get("studio"):
-			gallery["studio_id"] = self.studio_from_scrape(gallery["studio"]).get("id", None)
-			del gallery["studio"]
+			gallery_update["studio_id"] = self.studio_from_scrape(gallery["studio"]).get("id", None)
 			
-		return clean_dict(gallery)
+		return gallery_update
 
 	def studio_from_scrape(self, studio):
 		"""
@@ -122,17 +121,14 @@ class ScrapeParser:
 			aliases: [String!]
 		}
 		"""
-		# ignore field
-		if studio.get("remote_site_id"):
-			del studio["remote_site_id"]
+		studio_update = {}
 		
 		if studio.get("stored_id"):
-			studio["id"] = studio["stored_id"]
-			del studio["stored_id"]
+			studio_update["id"] = studio["stored_id"]
 		elif self.create_missing_studios:
 			return self.stash.create_studio(studio)
 
-		return clean_dict(studio)
+		return studio_update
 
 	def movie_from_scrape(self, movie):
 		"""
@@ -170,10 +166,11 @@ class ScrapeParser:
 		# NOTE
 		#  duration: String (HH:MM:SS) => duration: Int (Total Seconds)
 		#  studio: {ScrapedMovieStudio} => studio_id: ID
+		movie_update = {}
 		
-		if movie.get("studio"):
-			movie["studio_id"] = self.studio_from_scrape(movie["studio"])["id"]
-			del movie["studio"]
+		if movie.get("stored_id"):
+			movie_update["id"] = movie["stored_id"]
+			return movie_update
 
 		# duration value from scraped movie is string, update expects an int
 		if movie.get("duration"):
@@ -183,9 +180,17 @@ class ScrapeParser:
 				movie["duration"] = f'00:{movie["duration"]}'
 			h,m,s = movie["duration"].split(':')
 			duration = datetime.timedelta(hours=int(h),minutes=int(m),seconds=int(s)).total_seconds()
-			movie['duration'] = int(duration)
+			movie_update['duration'] = int(duration)
 
-		return clean_dict(movie)
+		if movie.get("studio"):
+			movie_update["studio_id"] = self.studio_from_scrape(movie["studio"])["id"]
+
+		for attr in ["name","aliases","date","rating","director","url","synopsis","front_image","back_image"]:
+			if movie.get(attr):
+				movie_update[attr] = movie[attr]
+
+		m = self.stash.find_movie(movie_update, create=True)
+		return {"movie_id": m["id"], "scene_index": movie.get("scene_index", None) }
 
 	def performer_from_scrape(self, performer):
 		"""
@@ -251,34 +256,36 @@ class ScrapeParser:
 		"""
 		# NOTE
 		# 	ScrapedPerformer.gender: String => PerformerCreateInput.gender: GenderEnum
-		#   ScrapedPerformer.weight: String (kg?) => PerformerCreateInput.weight: Int (kg)
+		#  ScrapedPerformer.weight: String (kg?) => PerformerCreateInput.weight: Int (kg)
+		performer_update = {}
 
 		if performer.get("stored_id"):
-			performer["id"] = performer["stored_id"]
-			del performer["stored_id"]
+			performer_update["id"] = performer["stored_id"]
+			return performer_update
 
 		if performer.get("images") and len(performer["images"]) > 0:
-			performer["image"] = performer["images"][0]
+			performer_update["image"] = performer["images"][0]
 			
 		if performer.get("tags"):
-			performer["tag_ids"] = [self.tag_from_scrape(t)["id"] for t in performer.get("tags")]
-			del performer["tags"]
+			performer_update["tag_ids"] = [self.tag_from_scrape(t)["id"] for t in performer.get("tags")]
 
 		if performer.get("weight"):
 			try:
-				performer["weight"] = int(performer["weight"])
+				performer_update["weight"] = int(performer["weight"])
 			except:
-				del performer["weight"]
 				log.warning(f'Could not parse performer weight "{performer["weight"]}" it Int for {performer["name"]}')
 
 		if performer.get("gender"):
 			try:
-				performer["gender"] = Gender[performer["gender"]].value
+				performer_update["gender"] = Gender[performer["gender"]].value
 			except:
-				del performer["gender"]
 				log.warning(f'Cannot map performer Gender "{performer["gender"]}" for {performer["name"]}')
 
-		return clean_dict(performer)
+		for attr in ["name","url","gender","birthdate","ethnicity","country","eye_color","height","measurements","fake_tits","career_length","tattoos","piercings","aliases","details","death_date","hair_color"]:
+			if performer.get(attr):
+				performer_update[attr] = performer[attr]
+
+		return performer_update
 
 	def scene_from_scrape(self, scene):
 		"""
@@ -316,51 +323,35 @@ class ScrapeParser:
 			stash_ids: [StashIDInput!]
 		}
 		"""
-
-		# ignore field
-		if scene.get("file"):
-			del scene["file"]
-		# ignore field
-		if scene.get("remote_site_id"):
-			del scene["remote_site_id"]
-		# ignore field
-		if scene.get("duration"):
-			del scene["duration"]
-		# ignore field
-		if scene.get("fingerprints"):
-			del scene["fingerprints"]
-
+		scene_update = {}
+		
 		if scene.get("image"):
-			scene["cover_image"] = scene["image"]
-			del scene["image"]
+			scene_update["cover_image"] = scene["image"]
 
 		if scene.get("studio"):
-			scene["studio_id"] = self.studio_from_scrape(scene["studio"]).get("id")
-			del scene["studio"]
+			scene_update["studio_id"] = self.studio_from_scrape(scene["studio"]).get("id")
 
 		if scene.get("tags"):
-			scene["tag_ids"] = [self.tag_from_scrape(t).get("id") for t in scene["tags"]]
-			del scene["tags"]
+			scene_update["tag_ids"] = [self.tag_from_scrape(t).get("id") for t in scene["tags"]]
 
 		if scene.get("performers"):
-			scene["performer_ids"] = []
+			scene_update["performer_ids"] = []
 			for p in scene["performers"]:
 				performer = self.performer_from_scrape(p)
 				if performer.get("id"):
-					scene["performer_ids"].append(performer["id"])
+					scene_update["performer_ids"].append(performer["id"])
 				else:
 					log.debug(f"could not match {p}")
-			del scene["performers"]
 
 		if scene.get("movies"):
-			scene["movies"] = [self.movie_from_scrape(m) for m in scene["movies"]]
+			movies = [self.movie_from_scrape(m) for m in scene["movies"]]
+			scene_update["movies"] = [{"stored_id": m["movie_id"]} for m in movies]
 
-		return clean_dict(scene)
+		return scene_update
 
 
 	def localize_scraped_scene(self, scraped_scene):
 		# casts ScrapedScene to ScrapedScene while resolving aliases
-
 		"""
 		v0.12.0-40
 		type ScrapedScene {
