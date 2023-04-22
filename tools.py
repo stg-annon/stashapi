e44c654fa263c8527e4806dce345d53cfe81975e
stg-annon <14135675+stg-annon@users.noreply.github.com>
2023-04-22 15:47:40 -0400

    update base64 tools

--- tools.py
+++ tools.py
@@ -1,4 +1,4 @@
-import base64, math 
+import base64, math
 from collections import defaultdict
 
 def defaultify(d:dict, default=None):
@@ -11,7 +11,18 @@ def clean_dict(to_clean):
 # returns dictionary where values are not None
 	return {k:v for k,v in to_clean.items() if v and "__" not in k}
 
-def get_base64(image_path):
+def get_base64(pth):
+	"""deprecated use file_to_base64() instead"""
+	return file_to_base64(pth)
+
+def url_to_base64(url):
+	import requests
+	return base64.b64encode(requests.get(url).content)
+
+def file_to_base64(image_path):
+
+	from pathlib import Path
+	import mimetypes
 	"""get base64 encoded image from local image path
 
 	Args:
@@ -19,12 +30,13 @@ def get_base64(image_path):
 
 	Returns:
 		 str: base64 encoded string of image
-	"""	
+	"""
+	mime =  mimetypes.types_map.get(Path(image_path).suffix, 'image/jpeg')
 	with open(image_path, "rb") as img:
 		b64img_bytes = base64.b64encode(img.read())
 	if not b64img_bytes:
 		return None
-	return f"data:image/jpeg;base64,{b64img_bytes.decode('utf-8')}"
+	return f"data:{mime};base64,{b64img_bytes.decode('utf-8')}"
 
 def si_prefix(size, round):
 	power_labels = {3:'K', 6:'M', 9:'G', 12:'T'}
